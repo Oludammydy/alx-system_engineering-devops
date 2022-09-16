a6df9394d0a11759a3dcddef38bc3607c656c66c
Oludammydy <oludammydy@gmail.com>
2022-09-16 16:26:45 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
+#!/usr/bin/pup
 # Installs flask from pip3, version 2.1.0
 
-exec { 'install flask':
-  command => 'sudo pip3 install flask -v 2.1.0',
-  path    => '/usr/bin',
-  }
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
