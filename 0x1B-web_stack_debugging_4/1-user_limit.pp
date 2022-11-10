abdab11990414437832d027cac09f80683786307
oludammydy <oludammydy@gmail.com>
2022-11-10 19:57:07 +0100

    User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Change the OS configuration login to use holberton user
+exec {'hard-type':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec {'soft-type':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
